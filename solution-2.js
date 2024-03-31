d9c6d907889135d270f66a52ea3017da10bb88ae
Martha <marthanyangatiwaithaka@gmail.com>
2024-03-31 11:59:43 +0300

    Updated file

---
+++ solution-2.js
@@ -0,0 +1,20 @@
+
+
+function generateArrayBetween(num1, num2) {
+    //represents the minimum number within the range
+    let start = Math.min(num1, num2);
+    //represents the maximum number within the range
+    let end = Math.max(num1, num2);
+    // Initialize an array to store the result
+    let array = [];
+    // Iterate through each number in the input range
+    for (let i = start; i <= end; i++) {
+        array.push(i);
+    }
+
+    return array;
+}
+
+// Test the function
+console.log(generateArrayBetween(4, 7)); // Output: [4, 5, 6, 7]
+console.log(generateArrayBetween(-4, 7)); // Output: [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7]
